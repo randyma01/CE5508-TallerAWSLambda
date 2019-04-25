fa932e531950467fd2a2d2ef4234d7287523bec5
Randy <randyma01@gmail.com>
2019-04-24 21:19:11 -0600

    contenido ejemplo 2 y 3

--- ejemplo_3_python/app.py
+++ ejemplo_3_python/app.py
@@ -1,1 +1,15 @@
-Print('Hola Mundo')
+import boto3
+
+dynamodb = boto3.resource('dynamodb')
+
+def insert_data(data):
+    table = dynamodb.Table('Test')
+    response = table.put_item(Item=data)
+
+
+def lambda_handler(event,context):
+    try:
+        insert_data(event)
+        return str("PutItem succeeded!")
+    except Exception as e:
+        return str(e)
